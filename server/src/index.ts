c55c3ac5b964a855c0d1954038e28d941be542e6
evantianx <evantianx@gmail.com>
2020-08-23 11:20:49 +0800

    feat: login cookies to store user sessions

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,13 +5,16 @@ import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./graphql";
 import { connectDB } from "./database";
+import cookieParser from "cookie-parser";
 
 const mount = async (app: Application) => {
   const db = await connectDB();
+
+  app.use(cookieParser(process.env.SECRET));
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db }),
+    context: ({ req, res }) => ({ db, req, res }),
   });
   server.applyMiddleware({ app, path: "/api" });
 
