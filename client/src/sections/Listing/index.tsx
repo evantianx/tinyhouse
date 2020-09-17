e7fb799c779c060cb70801ebc66ba1e7947d78ce
evantianx <evantianx@gmail.com>
2020-09-17 18:11:47 +0800

    feat[client]: listing graphql and listing page(WIP)

--- client/src/sections/Listing/index.tsx
+++ client/src/sections/Listing/index.tsx
@@ -1,3 +1,53 @@
-import React from "react";
+import { useQuery } from "@apollo/client";
+import React, { useState } from "react";
+import { Col, Row, Layout } from "antd";
+import { RouteComponentProps } from "react-router-dom";
+import { LISTING } from "../../lib/graphql/queries";
+import {
+  Listing as ListingData,
+  ListingVariables,
+} from "../../lib/graphql/queries/Listing/__generated__/Listing";
+import { PageSkeleton, ErrorBanner } from "../../lib/components";
 
-export const Listing = () => <h2>Listing</h2>;
+interface MatchParams {
+  id: string;
+}
+
+const { Content } = Layout;
+
+const PAGE_LIMIT = 3;
+export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
+  const [bookingsPage, setBookingsPage] = useState(1);
+  const { loading, error, data } = useQuery<ListingData, ListingVariables>(
+    LISTING,
+    {
+      variables: {
+        id: match.params.id,
+        bookingsPage,
+        limit: PAGE_LIMIT,
+      },
+    }
+  );
+
+  if (loading) {
+    return (
+      <Content className="listings">
+        <PageSkeleton />
+      </Content>
+    );
+  }
+
+  if (error) {
+    return (
+      <Content className="listings">
+        <ErrorBanner description="This listing may not exist or we've encountered an error. Please try again later." />
+        <PageSkeleton />
+      </Content>
+    );
+  }
+
+  const listing = data?.listing;
+  const listingBookings = data?.listing?.bookings;
+
+  return <h2>listing</h2>;
+};
