b584e7148cb4a56b2e043a497f0296ceaea4d8b9
evantianx <evantianx@gmail.com>
2020-09-18 20:41:58 +0800

    feat[client]: HomeHero component

--- client/src/sections/Home/index.tsx
+++ client/src/sections/Home/index.tsx
@@ -1,3 +1,24 @@
 import React from "react";
+import { Layout } from "antd";
+import { HomeHero } from "./components";
 
-export const Home = () => <h2>Home</h2>;
+import mapBackGround from "./assets/map-background.jpg";
+import { RouteComponentProps } from "react-router-dom";
+import { displayErrorMessage } from "../../lib/utils";
+
+const { Content } = Layout;
+export const Home = ({ history }: RouteComponentProps) => {
+  const onSearch = (value: string) => {
+    value.trim()
+      ? history.push(`listings/${value.trim()}`)
+      : displayErrorMessage("Please enter a valid search!");
+  };
+  return (
+    <Content
+      className="home"
+      style={{ backgroundImage: `url(${mapBackGround})` }}
+    >
+      <HomeHero onSearch={onSearch} />
+    </Content>
+  );
+};
