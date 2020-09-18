8c2ed5db5abb686d6d85a2b642d00ebb709aa15d
evantianx <evantianx@gmail.com>
2020-09-18 22:03:01 +0800

    feat[client]: HomeListings component

--- client/src/sections/Home/index.tsx
+++ client/src/sections/Home/index.tsx
@@ -1,21 +1,56 @@
 import React from "react";
 import { Col, Layout, Row, Typography } from "antd";
-import { HomeHero } from "./components";
+import { HomeHero, HomeListings, HomeListingsSkeleton } from "./components";
 import { Link, RouteComponentProps } from "react-router-dom";
 import { displayErrorMessage } from "../../lib/utils";
 
 import mapBackGround from "./assets/map-background.jpg";
 import sanFransiscoImage from "./assets/san-fransisco.jpg";
 import cancunImage from "./assets/cancun.jpg";
+import { useQuery } from "@apollo/client";
+import {
+  Listings,
+  ListingsVariables,
+} from "../../lib/graphql/queries/Listings/__generated__/Listings";
+import { LISTINGS } from "../../lib/graphql/queries";
+import { ListingFilter } from "../../lib/graphql/globalTypes";
 
 const { Content } = Layout;
 const { Paragraph, Title } = Typography;
 
+const PAGE_LIMIT = 4;
+const PAGE_NUMBER = 1;
+
 export const Home = ({ history }: RouteComponentProps) => {
+  const { loading, data } = useQuery<Listings, ListingsVariables>(LISTINGS, {
+    variables: {
+      filter: ListingFilter.PRICE_HIGH_TO_LOW,
+      limit: PAGE_LIMIT,
+      page: PAGE_NUMBER,
+    },
+  });
+
   const onSearch = (value: string) => {
     value.trim()
       ? history.push(`listings/${value.trim()}`)
       : displayErrorMessage("Please enter a valid search!");
+  };
+
+  const renderListingSection = () => {
+    if (loading) {
+      return <HomeListingsSkeleton />;
+    }
+
+    if (data) {
+      return (
+        <HomeListings
+          title="Premium Listings"
+          listings={data.listings.result}
+        />
+      );
+    }
+
+    return null;
   };
   return (
     <Content
@@ -38,6 +73,8 @@ export const Home = ({ history }: RouteComponentProps) => {
           Popular listings in the United States
         </Link>
       </div>
+
+      {renderListingSection()}
 
       <div>
         <Title level={4} className="home__listings-title">
