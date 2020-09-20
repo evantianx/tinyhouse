6f934e61bdfda068dd12e4a78dc9696507cbf174
evantianx <evantianx@gmail.com>
2020-09-20 10:11:50 +0800

    feat[client]: listings page listingsFilter compoent

--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout, List, Typography } from "antd";
 import { useQuery } from "@apollo/client";
 import {
@@ -9,6 +9,7 @@ import { LISTINGS } from "../../lib/graphql/queries";
 import { ListingFilter } from "../../lib/graphql/globalTypes";
 import { ListingCard } from "../../lib/components";
 import { Link, RouteComponentProps } from "react-router-dom";
+import { ListingsFilterSection } from "./components";
 
 const { Content } = Layout;
 const { Title, Paragraph, Text } = Typography;
@@ -19,47 +20,56 @@ interface MatchParams {
 }
 
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
-  const { data } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
-    variables: {
-      location: match.params.location,
-      filter: ListingFilter.PRICE_LOW_TO_HIGH,
-      limit: PAGE_LIMIT,
-      page: 1,
-    },
-  });
+  const [filter, setFilter] = useState(ListingFilter.PRICE_LOW_TO_HIGH);
+  const { data, loading } = useQuery<ListingsData, ListingsVariables>(
+    LISTINGS,
+    {
+      variables: {
+        location: match.params.location,
+        limit: PAGE_LIMIT,
+        filter,
+        page: 1,
+      },
+    }
+  );
 
   const listings = data?.listings;
   const listingsRegion = listings?.region;
 
   const listingsSectionElement =
     listings && listings.result.length ? (
-      <List
-        grid={{
-          gutter: 8,
-          xs: 1,
-          sm: 2,
-          lg: 4,
-          xl: 4,
-          xxl: 4,
-        }}
-        dataSource={listings.result}
-        renderItem={(listing) => (
-          <List.Item>
-            <ListingCard listing={listing} />
-          </List.Item>
-        )}
-      />
+      <>
+        <ListingsFilterSection filter={filter} setFilter={setFilter} />
+        <List
+          grid={{
+            gutter: 8,
+            xs: 1,
+            sm: 2,
+            lg: 4,
+            xl: 4,
+            xxl: 4,
+          }}
+          dataSource={listings.result}
+          renderItem={(listing) => (
+            <List.Item>
+              <ListingCard listing={listing} />
+            </List.Item>
+          )}
+        />
+      </>
     ) : (
-      <div>
-        <Paragraph>
-          It appears that no listings have been created for{""}
-          <Text mark>{listingsRegion}</Text>
+      !loading && (
+        <div>
           <Paragraph>
-            Be the first person to create a{" "}
-            <Link to="/host">listing in this area</Link>
+            It appears that no listings have been created for{""}
+            <Text mark>{listingsRegion}</Text>
+            <Paragraph>
+              Be the first person to create a{" "}
+              <Link to="/host">listing in this area</Link>
+            </Paragraph>
           </Paragraph>
-        </Paragraph>
-      </div>
+        </div>
+      )
     );
 
   const listingsRegionElement = listingsRegion ? (
