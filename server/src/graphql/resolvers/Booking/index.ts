2c0f9d46d0782048b48b50f46f923e8bcd61dcef
evantianx <evantianx@gmail.com>
2020-09-24 10:18:05 +0800

    feat[server]: createbooking resolver

--- server/src/graphql/resolvers/Booking/index.ts
+++ server/src/graphql/resolvers/Booking/index.ts
@@ -1,7 +1,157 @@
 import { IResolvers } from "apollo-server-express";
-import { Booking, Database, Listing } from "../../../lib/types";
+import { Booking, BookingsIndex, Database, Listing } from "../../../lib/types";
+import { authorize } from "../../../lib/utils";
+import { CreateBookingArgs } from "./types";
+import { Request } from "express";
+import { ObjectId } from "mongodb";
+import { Stripe } from "../../../lib/api";
+
+const resolveBookinsIndex = (
+  bookingsIndex: BookingsIndex,
+  checkInDate: string,
+  checkOutDate: string
+): BookingsIndex => {
+  const checkOut = new Date(checkOutDate);
+  let dateCursor = new Date(checkInDate);
+
+  const newBookingsIndex: BookingsIndex = { ...bookingsIndex };
+
+  while (dateCursor <= checkOut) {
+    const y = dateCursor.getUTCFullYear();
+    const m = dateCursor.getUTCMonth();
+    const d = dateCursor.getUTCDate();
+
+    if (!newBookingsIndex[y]) {
+      newBookingsIndex[y] = {};
+    }
+    if (!newBookingsIndex[y][m]) {
+      newBookingsIndex[y][m] = {};
+    }
+    if (!newBookingsIndex[y][m][d]) {
+      newBookingsIndex[y][m][d] = true;
+    } else {
+      throw new Error(
+        "selected dates can't overlap dates that have already been booked"
+      );
+    }
+    dateCursor = new Date(dateCursor.getTime() + 86400000);
+  }
+
+  return newBookingsIndex;
+};
 
 export const bookingResolvers: IResolvers = {
+  Mutation: {
+    createBooking: async (
+      _root: undefined,
+      { input }: CreateBookingArgs,
+      { db, req }: { db: Database; req: Request }
+    ): Promise<Booking> => {
+      try {
+        const { id, source, checkIn, checkOut } = input;
+
+        // verify a logged in user is making request
+        const viewer = await authorize(db, req);
+        if (!viewer) {
+          throw new Error("viewer cannot be found");
+        }
+
+        // find listing document that is being booked
+        const listing = await db.listings.findOne({
+          _id: new ObjectId(id),
+        });
+        if (!listing) {
+          throw new Error("listing cannot be found");
+        }
+
+        // check that viewer is Not booking their own listing
+        if (listing.host === viewer._id) {
+          throw new Error("viewer cannot book own listing");
+        }
+
+        // check that checkOut is Not before checkIn
+        const checkInDate = new Date(checkIn);
+        const checkOutDate = new Date(checkOut);
+
+        if (checkOutDate < checkInDate) {
+          throw new Error("check out date cannot be before check in date");
+        }
+
+        // create a new bookingIndex for listing being booked
+        const bookingsIndex = resolveBookinsIndex(
+          listing.bookingsIndex,
+          checkIn,
+          checkOut
+        );
+
+        // get total price to charge
+        const totalPrice =
+          listing.price *
+          ((checkOutDate.getTime() - checkOutDate.getTime()) / 86400000 + 1);
+
+        // get user document of host of listing
+        const host = await db.users.findOne({
+          _id: listing.host,
+        });
+
+        if (!host || !host.walletId) {
+          throw new Error(
+            "the host either cannot be found or is not connected with Stripe"
+          );
+        }
+
+        // create stripe charge on behalf of host
+        await Stripe.charge(totalPrice, source, host.walletId);
+
+        // insert a new booking document to bookings collection
+        const insertRes = await db.bookings.insertOne({
+          _id: new ObjectId(),
+          listing: listing._id,
+          tenant: viewer._id,
+          checkIn,
+          checkOut,
+        });
+
+        // update user document of host to increment income
+        const insertedBooking: Booking = insertRes.ops[0];
+
+        await db.users.updateOne(
+          {
+            _id: host._id,
+          },
+          {
+            $inc: { income: totalPrice },
+          }
+        );
+
+        // update bookings field of tenant
+        await db.users.updateOne(
+          {
+            _id: viewer._id,
+          },
+          {
+            $push: { bookings: insertedBooking._id },
+          }
+        );
+
+        // upadte bookings field of listing document
+        await db.listings.updateOne(
+          {
+            _id: listing._id,
+          },
+          {
+            $set: { bookingsIndex },
+            $push: { bookings: insertedBooking._id },
+          }
+        );
+
+        // return newly inserted booking
+        return insertedBooking;
+      } catch (err) {
+        throw new Error(`Failed to create a booking: ${err}`);
+      }
+    },
+  },
   Booking: {
     id: (booking: Booking): string => {
       return booking._id.toString();
