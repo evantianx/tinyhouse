f17f8c010136ff4a626340014c352e28bab48d82
evantianx <evantianx@gmail.com>
2020-09-25 12:36:14 +0800

    feat[client]: not found page

--- client/src/sections/NotFound/index.tsx
+++ client/src/sections/NotFound/index.tsx
@@ -1,3 +1,31 @@
 import React from "react";
+import { Empty, Layout, Typography } from "antd";
+import { Link } from "react-router-dom";
 
-export const NotFound = () => <h2>NotFound</h2>;
+const { Content } = Layout;
+const { Text } = Typography;
+
+export const NotFound = () => {
+  return (
+    <Content className="not-found">
+      <Empty
+        description={
+          <>
+            <Text className="not-found__description-title">
+              Uh oh! Something went wrong :(
+            </Text>
+            <Text className="not-found__description-subtitle">
+              The page you're looking for can't be found
+            </Text>
+          </>
+        }
+      />
+      <Link
+        to="/"
+        className="not-found__cta ant-btn ant-btn-primary ant-btn-lg"
+      >
+        Go to home
+      </Link>
+    </Content>
+  );
+};
